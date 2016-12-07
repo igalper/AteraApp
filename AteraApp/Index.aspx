dce87558bf27ecf140cf7e6e7202c754ba7889cf
igalpe <igalpe@IGAL-PC>
2016-12-07 23:36:42 +0200

    Add project files.

---
+++ AteraApp/Index.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Index.aspx.cs" Inherits="AteraApp.Index" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <h1>Devices Page</h1>
+    </div>
+    <div>
+
+
+        <form class="float_left" action="/Devices/Test" method="get">
+            <button  type="submit">Go</button>
+        </form>
+
+    </div>
+    <div id="device-list-div">
+        
+    </div>
+    </form>
+</body>
+</html>
---
+++ DAL/DataQueries.cs
@@ -0,0 +1,53 @@
+﻿using DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class DataQueries
+    {
+        public int GetNumberOfDevices()
+        {
+            using (var db = new MasterEntities())
+            {
+                int count = 0;
+                foreach (Device d in db.Devices)
+                    count++;
+
+                return count;
+            }
+        }
+
+        public List<Device> GetDevicesByOwnerName(string ownerName)
+        {
+            List<Device> devicesList = new List<Device>();
+
+            using (var db = new MasterEntities())
+            {
+                devicesList =
+                    (from d in db.Devices
+                     where d.Owner == ownerName       
+                     select d).ToList();
+            }
+
+            return devicesList;
+        }
+
+        public List<Device> GetAllDevices()
+        {
+            List<Device> devicesList = new List<Device>();
+
+            using (var db = new MasterEntities())
+            {
+                devicesList =
+                    (from d in db.Devices
+                     select d).ToList();
+            }
+
+            return devicesList;
+        }
+    }
+}
