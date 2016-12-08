b67995f3a66385c814e4ba147a2f486974a46096
igalpe <igalpe@IGAL-PC>
2016-12-08 12:24:25 +0200

    2 post methods

--- AteraApp/Controller/DevicesController.cs
+++ AteraApp/Controller/DevicesController.cs
@@ -4,17 +4,41 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using DAL.Models;
+
+
 
 namespace AteraApp
 {
     public class DevicesController : ApiController
     {
-        [Route("Devices/Test")]
-        [HttpGet]
-        public string Test()
+        [Route("Devices")]
+        [HttpPost]
+        public string getAllDevices()
         {
-            return "Its Working!";
+            var dataQueries = new DAL.DataQueries();
+            var res = "";
+            foreach (Device d in dataQueries.GetAllDevices())
+            {
+                res += "{" + d.ToString()+" },";
+            }
+            
+            return res;
         }
+        [Route("Devices/{id}")]
+        [HttpPost]
+        public string getDevice([FromUri] int id)
+        {
+            var dataQueries = new DAL.DataQueries();
+            var res = "";
+            List<Device> deviceList = dataQueries.GetAllDevices();
+            Device d = deviceList.Find(device => device.Id == id);
+            res += "{" + d.ToString() + " }";
+            
+
+            return res;
+        }
+
 
         // GET api/<controller>
         /*public IEnumerable<string> Get()
@@ -29,9 +53,9 @@ namespace AteraApp
         }*/
 
         // POST api/<controller>
-        public void Post([FromBody]string value)
+        /*public void Post([FromBody]string value)
         {
-        }
+        }*/
 
         // PUT api/<controller>/5
         public void Put(int id, [FromBody]string value)
